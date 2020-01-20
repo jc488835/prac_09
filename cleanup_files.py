aca3d2a91420fb62c7c4e96781ac63826a008044
jc488835 <chunjie.chen@my.jcu.edu.au>
2020-01-20 19:12:22 +0800

    Initial commit

---
+++ cleanup_files.py
@@ -0,0 +1,20 @@
+import os
+
+
+def main():
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        for filename in filenames:
+            new_name = get_fixed_filename(filename)
+            print("Renaming {} to {}".format(filename, new_name))
+            original_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            os.rename(original_name, new_name)
+
+
+def get_fixed_filename(filename):
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    return new_name
+
+
+main()
